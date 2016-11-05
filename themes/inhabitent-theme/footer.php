f08a893000eaa79fe416b583e2b785d2984a5b01
shamu92 <altshteyn@gmail.com>
2016-11-05 15:02:19 -0700

    added hero image

--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -21,6 +21,8 @@
 						</p>
 						<p>
 							<i class="fa fa-facebook-square" aria-hidden="true"></i>
+							<i class="fa fa-twitter-square" aria-hidden="true"></i>
+							<i class="fa fa-google-plus-square" aria-hidden="true"></i>
 						</p>
 					</div>
 					<div class = "business-hours">
--- themes/inhabitent-theme/header.php
+++ themes/inhabitent-theme/header.php
@@ -31,16 +31,16 @@
 				
 
 				
-					<!-- #site-navigation -->
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<!--Header Logo -->
-					<a href = "http://localhost/inhabitent/"><img class ="logo-top" src = "<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-tent.svg" alt = "inhabitent logo"></a>
-					<!--End of header Logo -->
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				<div class="header">	<!-- #site-navigation -->
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+						<!--Header Logo -->
+						<a href = "http://localhost/inhabitent/"><img class ="logo-top" src = "<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-tent.svg" alt = "inhabitent logo"></a>
+						<!--End of header Logo -->
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					
-				</nav><!-- #site-navigation -->
-				
+					</nav><!-- #site-navigation -->
+				</div>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
