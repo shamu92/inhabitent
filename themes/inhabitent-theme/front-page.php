4d058d63951e0c6a1354230693da257b2dbd0c74
shamu92 <altshteyn@gmail.com>
2016-11-05 19:14:31 -0700

    added fonts files and adventure section

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -22,4 +22,33 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+<div>
+    <h2 class ="latest-adventures">Latest Adventures</h2>
+
+    <section class= "adventures">
+        <div class="box-1">
+            <a class = "adventure-link" href ="#">Getting Back to Nature in a Canoe</a>
+            <a class = "read-more-button" href ="#">Read More</a>
+        </div>
+        <div class="right-box">
+            <div class="box-2">
+                <a class = "adventure-link" href ="#">A night with Friends at the Beach</a><br>
+                <a class = "read-more-button" href ="#">Read More</a>
+
+            </div>
+            <div class = "bottom-right-box">  
+                <div class="box-3">
+                    <a class = "adventure-link" href ="#">Taking in the View at Big Mountain</a>
+                    <a class = "read-more-button" href ="#">Read More</a>
+
+                </div>
+                <div class="box-4">
+                    <a class = "adventure-link" href ="#">Star-Gazing at the Night Sky</a>
+                    <a class = "read-more-button" href ="#">Read More</a>
+
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 <?php get_footer(); ?>
