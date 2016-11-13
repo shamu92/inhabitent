1bf6b4962d805c2f3a587ba0efe09b400c10c37b
shamu92 <altshteyn@gmail.com>
2016-11-12 19:05:43 -0800

    styled 404

--- themes/inhabitent-theme/search.php
+++ themes/inhabitent-theme/search.php
@@ -13,7 +13,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<h1 class="page-title-search"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
@@ -32,7 +32,8 @@ get_header(); ?>
 		<?php endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
 
 <?php get_sidebar(); ?>
+	</section><!-- #primary -->
+
 <?php get_footer(); ?>
